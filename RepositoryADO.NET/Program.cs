79c70cf6700c78d2967a77179e452ec8336e578f
Saveliy Kolesnikov <s.koles33@gmail.com>
2018-08-01 21:28:07 +0300

    Add project files.

---
+++ RepositoryADO.NET/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RepositoryADO.NET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
